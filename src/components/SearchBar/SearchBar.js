d545b0523b18cd6786f5bc8e4f344bfe3dd8f157
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-08-08 08:55:44 -0300

    improvements on searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,52 @@
 import React from "react";
 import "./SearchBar.css";
 
-let sortByOptions = {
-    'Best Match' : 'best_match',
-    'Highest Rated' : 'rating',
-    'Most Reviewed' : 'review_count'
-}
-
 class SearchBar extends React.Component {
-    renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+    constructor (props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        }
+        this.sortByOptions = {
+            'Best Match' : 'best_match',
+            'Highest Rated' : 'rating',
+            'Most Reviewed' : 'review_count'
+        }
+    }
+
+    handleSortByChange(sortByOption){
+        this.setState({
+            sortBy: sortByOption
         });
     }
+
+    getSortByClass(sortByOption){
+        if(this.state.sortBy === sortByOption){
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+    
+
+    renderSortByOptions(){
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return (<li className={this.getSortByClass(sortByOptionValue)}
+                       key={sortByOptionValue}     
+                       onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+                        {sortByOption}
+                        </li>);
+        });
+    }
+
+    /*Double bind: This will allow us to both bind to the current value of 
+    this (as we usually do in the constructor()) but also bind 
+    the current sortByOptionValue as the first argument to the method call, 
+    ensuring the method is called with the appropriate value when clicked. */
+
     render(){
         return (
             <div className="SearchBar">
